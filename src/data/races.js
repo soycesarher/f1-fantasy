bbc8a46abc68569b8d686d5adb58f209c082af5d
César Hernández <79117401+soycesarher@users.noreply.github.com>
2026-03-09 13:57:44 -0600

    Update races.js

--- src/data/races.js
+++ src/data/races.js
@@ -3,194 +3,186 @@
 export const races2026 = [
   {
     id: 1,
-    name: "Gran Premio de Australia",
-    circuit: "Albert Park Circuit",
-    date: "2026-03-08",
-    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmelbournedetailed.webp",
-    isOpen: false
-  },
-  {
-    id: 2,
     name: "Gran Premio de China",
     circuit: "Shanghai International Circuit",
-    date: "2026-03-15",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Shanghai_International_Racing_Circuit_track_map.svg/1024px-Shanghai_International_Racing_Circuit_track_map.svg.png",
+    date: "2026-03-08",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackshanghaidetailed.webp",
     isOpen: true
   },
   {
-    id: 3,
+    id: 2,
     name: "Gran Premio de Japón",
     circuit: "Suzuka Circuit",
-    date: "2026-03-29",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Suzuka_circuit_map--2005.svg/1024px-Suzuka_circuit_map--2005.svg.png",
+    date: "2026-03-22",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026tracksuzukadetailed.webp",
+    isOpen: false
+  },
+  {
+    id: 3,
+    name: "Gran Premio de Bahrein",
+    circuit: "Bahrain International Circuit",
+    date: "2026-04-05",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026tracksakhirdetailed.webp",
     isOpen: false
   },
   {
     id: 4,
-    name: "Gran Premio de Bahrein",
-    circuit: "Bahrain International Circuit",
+    name: "Gran Premio de Arabia Saudita",
+    circuit: "Jeddah Corniche Circuit",
     date: "2026-04-12",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Bahrain_International_Circuit--Grand_Prix_Layout.svg/1280px-Bahrain_International_Circuit--Grand_Prix_Layout.svg.png",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackjeddahdetailed.webp",
     isOpen: false
   },
   {
     id: 5,
-    name: "Gran Premio de Arabia Saudita",
-    circuit: "Jeddah Corniche Circuit",
-    date: "2026-04-19",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Jeddah_Corniche_Circuit_2021.svg/1024px-Jeddah_Corniche_Circuit_2021.svg.png",
+    name: "Gran Premio de Miami",
+    circuit: "Miami International Autodrome",
+    date: "2026-05-03",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmiamidetailed.webp",
     isOpen: false
   },
   {
     id: 6,
-    name: "Gran Premio de Miami",
-    circuit: "Miami International Autodrome",
-    date: "2026-05-03",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Miami_International_Autodrome.svg/1024px-Miami_International_Autodrome.svg.png",
+    name: "Gran Premio de Canadá",
+    circuit: "Circuit Gilles Villeneuve",
+    date: "2026-05-24",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmontrealdetailed.webp",
     isOpen: false
   },
   {
     id: 7,
-    name: "Gran Premio de Canadá",
-    circuit: "Circuit Gilles Villeneuve",
-    date: "2026-05-24",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Circuit_Gilles_Villeneuve.svg/1024px-Circuit_Gilles_Villeneuve.svg.png",
+    name: "Gran Premio de Mónaco",
+    circuit: "Circuit de Monaco",
+    date: "2026-06-07",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmontecarlodetailed.webp",
     isOpen: false
   },
   {
     id: 8,
-    name: "Gran Premio de Mónaco",
-    circuit: "Circuit de Monaco",
-    date: "2026-06-07",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Monte_Carlo_Formula_1_track_map.svg/1024px-Monte_Carlo_Formula_1_track_map.svg.png",
+    name: "Gran Premio de España",
+    circuit: "Circuit de Barcelona-Catalunya",
+    date: "2026-06-14",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackcatalunyadetailed.webp",
     isOpen: false
   },
   {
     id: 9,
-    name: "Gran Premio de España",
-    circuit: "Circuit de Barcelona-Catalunya",
-    date: "2026-06-14",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Circuit_de_Barcelona-Catalunya.svg/1024px-Circuit_de_Barcelona-Catalunya.svg.png",
+    name: "Gran Premio de Austria",
+    circuit: "Red Bull Ring",
+    date: "2026-06-28",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackspielbergdetailed.webp",
     isOpen: false
   },
   {
     id: 10,
-    name: "Gran Premio de Austria",
-    circuit: "Red Bull Ring",
-    date: "2026-06-28",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Red_Bull_Ring_2022.svg/1024px-Red_Bull_Ring_2022.svg.png",
+    name: "Gran Premio de Gran Bretaña",
+    circuit: "Silverstone Circuit",
+    date: "2026-07-05",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026tracksilverstonedetailed.webp",
     isOpen: false
   },
   {
     id: 11,
-    name: "Gran Premio de Gran Bretaña",
-    circuit: "Silverstone Circuit",
-    date: "2026-07-05",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Silverstone_Circuit_2011.svg/1024px-Silverstone_Circuit_2011.svg.png",
+    name: "Gran Premio de Bélgica",
+    circuit: "Circuit de Spa-Francorchamps",
+    date: "2026-07-19",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackspafrancorchampsdetailed.webp",
     isOpen: false
   },
   {
     id: 12,
-    name: "Gran Premio de Bélgica",
-    circuit: "Circuit de Spa-Francorchamps",
-    date: "2026-07-19",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Spa-Francorchamps_of_Belgium.svg/1024px-Spa-Francorchamps_of_Belgium.svg.png",
+    name: "Gran Premio de Hungría",
+    circuit: "Hungaroring",
+    date: "2026-07-26",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackhungaroringdetailed.webp",
     isOpen: false
   },
   {
     id: 13,
-    name: "Gran Premio de Hungría",
-    circuit: "Hungaroring",
-    date: "2026-07-26",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Hungaroring.svg/1024px-Hungaroring.svg.png",
+    name: "Gran Premio de Países Bajos",
+    circuit: "Circuit Zandvoort",
+    date: "2026-08-23",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackzandvoortdetailed.webp",
     isOpen: false
   },
   {
     id: 14,
-    name: "Gran Premio de Países Bajos",
-    circuit: "Circuit Zandvoort",
-    date: "2026-08-23",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Circuit_Zandvoort-2020.svg/1024px-Circuit_Zandvoort-2020.svg.png",
+    name: "Gran Premio de Italia",
+    circuit: "Autodromo Nazionale Monza",
+    date: "2026-09-06",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmonzadetailed.webp",
     isOpen: false
   },
   {
     id: 15,
-    name: "Gran Premio de Italia",
-    circuit: "Autodromo Nazionale Monza",
-    date: "2026-09-06",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Autodromo_Nazionale_Monza_track_map.svg/1024px-Autodromo_Nazionale_Monza_track_map.svg.png",
+    name: "Gran Premio de Madrid",
+    circuit: "Madring",
+    date: "2026-09-13",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmadringdetailed.webp",
     isOpen: false
   },
   {
     id: 16,
-    name: "Gran Premio de España (Madrid)",
-    circuit: "Madrid Street Circuit",
-    date: "2026-09-13",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Circuito_de_Madrid_2026.svg/1024px-Circuito_de_Madrid_2026.svg.png",
+    name: "Gran Premio de Azerbaiyán",
+    circuit: "Baku City Circuit",
+    date: "2026-09-27",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackbakudetailed.webp",
     isOpen: false
   },
   {
     id: 17,
-    name: "Gran Premio de Azerbaiyán",
-    circuit: "Baku City Circuit",
-    date: "2026-09-26",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Baku_City_Circuit_2021.svg/1024px-Baku_City_Circuit_2021.svg.png",
+    name: "Gran Premio de Singapur",
+    circuit: "Marina Bay Street Circuit",
+    date: "2026-10-11",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026tracksingaporedetailed.webp",
     isOpen: false
   },
   {
     id: 18,
-    name: "Gran Premio de Singapur",
-    circuit: "Marina Bay Street Circuit",
-    date: "2026-10-11",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Marina_Bay_Street_Circuit_2023.svg/1024px-Marina_Bay_Street_Circuit_2023.svg.png",
+    name: "Gran Premio de Estados Unidos",
+    circuit: "Circuit of the Americas",
+    date: "2026-10-25",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackaustindetailed.webp",
     isOpen: false
   },
   {
     id: 19,
-    name: "Gran Premio de Estados Unidos",
-    circuit: "Circuit of the Americas",
-    date: "2026-10-25",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Circuit_of_the_Americas.svg/1024px-Circuit_of_the_Americas.svg.png",
+    name: "Gran Premio de México",
+    circuit: "Autódromo Hermanos Rodríguez",
+    date: "2026-11-01",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackmexicocitydetailed.webp",
     isOpen: false
   },
   {
     id: 20,
-    name: "Gran Premio de México",
-    circuit: "Autódromo Hermanos Rodríguez",
-    date: "2026-11-01",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Autodromo_Hermanos_Rodriguez_2015.svg/1024px-Autodromo_Hermanos_Rodriguez_2015.svg.png",
+    name: "Gran Premio de Brasil",
+    circuit: "Interlagos",
+    date: "2026-11-08",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackinterlagosdetailed.webp",
     isOpen: false
   },
   {
     id: 21,
-    name: "Gran Premio de Brasil",
-    circuit: "Interlagos",
-    date: "2026-11-08",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Autodromo_Jose_Carlos_Pace_aka_Interlagos.svg/1024px-Autodromo_Jose_Carlos_Pace_aka_Interlagos.svg.png",
+    name: "Gran Premio de Las Vegas",
+    circuit: "Las Vegas Strip Circuit",
+    date: "2026-11-21",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026tracklasvegasdetailed.webp",
     isOpen: false
   },
   {
     id: 22,
-    name: "Gran Premio de Las Vegas",
-    circuit: "Las Vegas Strip Circuit",
-    date: "2026-11-21",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Las_Vegas_Strip_Circuit_2023.svg/1024px-Las_Vegas_Strip_Circuit_2023.svg.png",
+    name: "Gran Premio de Qatar",
+    circuit: "Lusail International Circuit",
+    date: "2026-11-29",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026tracklusaildetailed.webp",
     isOpen: false
   },
   {
     id: 23,
-    name: "Gran Premio de Qatar",
-    circuit: "Lusail International Circuit",
-    date: "2026-11-29",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Lusail_International_Circuit_2023.svg/1024px-Lusail_International_Circuit_2023.svg.png",
-    isOpen: false
-  },
-  {
-    id: 24,
     name: "Gran Premio de Abu Dhabi",
     circuit: "Yas Marina Circuit",
     date: "2026-12-06",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Yas_Marina_Circuit.svg/1024px-Yas_Marina_Circuit.svg.png",
+    image: "https://media.formula1.com/image/upload/c_fit,h_704/q_auto/v1740000000/common/f1/2026/track/2026trackyasmarinacircuitdetailed.webp",
     isOpen: false
   }
 ];
@@ -198,6 +190,7 @@ export const races2026 = [
 export function getCurrentRace() {
   return races2026[0]; 
 }
+
 
 
 
